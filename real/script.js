00aabb7b024cd5425e8aa4efa7f799ef2e9549cc
Jerim1807 <125331274+Jerim1807@users.noreply.github.com>
2023-10-21 17:35:32 +0700

    Add files via upload

---
+++ real/script.js
@@ -0,0 +1,113 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('selectedMovie');
+const confirmButton = document.querySelector('.confirm-button');
+
+populateUI();
+
+let ticketPrice1 = +localStorage.getItem('selectedMoviePrice');
+let ticketPrice2 = ticketPrice1+20;
+
+// Function to get URL parameters by name
+function getURLParameter(name) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name);
+}
+
+// Get the selected movie name from the URL
+const selectedMovie = getURLParameter("movie");
+
+// Update the selected movie element
+if (movieSelect) {
+  movieSelect.textContent = selectedMovie || "Unknown Movie";
+}
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const seatNo = [...selectedSeats].map((seat) => seat.getAttribute('name'));
+  const selectedSeatsAB = [...selectedSeats].filter(seat => {
+    const seatName = seat.getAttribute('name');
+    return seatName && (seatName.startsWith('A') || seatName.startsWith('B'));
+  });
+
+  const selectedSeatsOther = [...selectedSeats].filter(seat => {
+    const seatName = seat.getAttribute('name');
+    return !seatName || (!seatName.startsWith('A') && !seatName.startsWith('B'));
+  });
+
+  const seatsIndexAB = selectedSeatsAB.map(seat => [...seats].indexOf(seat));
+  const seatsIndexOther = selectedSeatsOther.map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify([...seatsIndexAB, ...seatsIndexOther]));
+
+  const selectedSeatsCountAB = selectedSeatsAB.length;
+  const selectedSeatsCountOther = selectedSeatsOther.length;
+
+  count.innerText = seatNo;
+  total.innerText = (selectedSeatsCountOther * ticketPrice1) + (selectedSeatsCountAB * ticketPrice2);
+}
+
+// Get data from local storage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+// Movie select event
+movieSelect.addEventListener('change', (e) => {
+  const selectedMovieIndex = e.target.selectedIndex;
+  const selectedMoviePrice = e.target.value;
+  setMovieData(selectedMovieIndex, selectedMoviePrice);
+  updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener('click', (e) => {
+  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected');
+    updateSelectedCount();
+  }
+});
+
+// Confirm booking button click event
+confirmButton.addEventListener('click', () => {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  selectedSeats.forEach((seat) => {
+    seat.classList.remove('selected');
+    seat.classList.add('occupied');
+  });
+  // window.location.href = "";
+  updateSelectedCount();
+});
+
+const ticketPriceE1 = document.getElementById('ticketPrice1');
+// Update the small element with the ticket price
+if (ticketPriceE1) {
+  ticketPriceE1.textContent = ticketPrice1;
+}
+const ticketPriceE2 = document.getElementById('ticketPrice2');
+// Update the small element with the ticket price
+if (ticketPriceE2) {
+  ticketPriceE2.textContent = ticketPrice2;
+}
+
+
---
+++ real/transferScript.js
@@ -0,0 +1,21 @@
+document.getElementById("myButton1").addEventListener("click", function () {
+    // Set the selected movie and its price in the localStorage
+    //localStorage.setItem('selectedCinema', 1);
+    localStorage.setItem('selectedMovie', 'Avenger');
+    localStorage.setItem('selectedMoviePrice', 100);
+    window.location.href = "Booking.html?movie=Avenger";
+});
+document.getElementById("myButton2").addEventListener("click", function () {
+    // Set the selected movie and its price in the localStorage
+    //localStorage.setItem('selectedCinema', 2);
+    localStorage.setItem('selectedMovie', 'Joker');
+    localStorage.setItem('selectedMoviePrice', 120);
+    window.location.href = "Booking.html?movie=Joker";
+});
+document.getElementById("myButton3").addEventListener("click", function () {
+    // Set the selected movie and its price in the localStorage
+    //localStorage.setItem('selectedCinema', 3);
+    localStorage.setItem('selectedMovie', 'Toy');
+    localStorage.setItem('selectedMoviePrice', 110);
+    window.location.href = "Booking.html?movie=Toy";
+});
